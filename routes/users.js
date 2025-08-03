59ead2788ca3c9056d282f1eebac63b987385c38
GianPaglieri <paglierigianfranco@gmail.com>
2025-08-03 17:46:53 -0300

    protect user list

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { createUser, getUsers, requestPasswordReset, resetPassword } = require('../controllers/userController');
+const { obtenerUserIdDesdeRequest } = require('../middleware/authMiddleware');
+
+// Middleware sencillo para exigir autenticación
+const requireAuth = (req, res, next) => {
+  const userId = obtenerUserIdDesdeRequest(req, res);
+  if (!userId) return;
+  req.userId = userId;
+  next();
+};
 
 // Define las rutas usando las funciones importadas directamente
-router.get('/', getUsers);
+router.get('/', requireAuth, getUsers);
 router.post('/register', createUser);
 
 router.post('/request-password-reset', requestPasswordReset);
 router.post('/reset-password', resetPassword);
-
 
 module.exports = router;
